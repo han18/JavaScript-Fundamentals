e72db16c17aec6f2dc7234d6c1994afb0c6a74f0
han18 <han.fadah@gmail.com>
2023-11-28 18:49:22 -0500

    created the full data structure of objects and arrays

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("hello");
+
+const courseInfo = {
+  id: 451,
+  name: "introduction to JavaScript",
+};
+
+const assignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignmentsInfo: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
