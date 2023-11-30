1bb5fb13eba197f176f62c690332b3384201b1bf
han18 <han.fadah@gmail.com>
2023-11-30 07:18:26 -0500

    was able to add score with possible points

--- second.js
+++ second.js
@@ -17,6 +17,34 @@ console.log(callCourse());
 /////======================
 console.log("=========================");
 
+// an object of the assignment course and an [array] of assignment info
+const assignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
 console.log("======================================");
 // an array of objects
 const LearnerSubmissions = [
@@ -124,3 +152,8 @@ const addThem =
   LearnerSubmissions[1].submission.score;
 
 console.log(addThem);
+
+let addThem2 =
+  LearnerSubmissions[0].submission.score +
+  assignmentGroup.assignments[0].points_possible;
+console.log(addThem2);
